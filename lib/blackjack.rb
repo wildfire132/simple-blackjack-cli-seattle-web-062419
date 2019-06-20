9fe21c7212a8edc85ec5444ff585e34b549f86c1
Katana Tran <tkatyk21@gmail.com>
2019-06-20 03:59:29 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -33,9 +33,11 @@ def hit?(card_total)
   get_user_input
   if get_user_input == "h"
     deal_card
+    display_card_total
   elsif get_user_input != "h" && get_user_input != "s"
     invalid_command
     prompt_user
+  
   end
   display_card_total(card_total)
 end
